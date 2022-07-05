69707b4bf740734a057e1d48601996be58db67cd
Kahr Lu <kahr@planetart.com>
2022-07-05 15:37:20 +0800

    element-plus & auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   server: {
     port: 4000,
-    open: true, 
-    cors: true, 
+    open: true,
+    cors: true,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
-        changeOrigin: true, 
+        changeOrigin: true,
         secure: false,
-        rewrite: path => path.replace('^/api', '/api')
-      }
-    }
-   }
+        rewrite: path => path.replace('^/api', '/api'),
+      },
+    },
+  },
 })
